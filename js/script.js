9d9d9d7a3058cd74a14e58dc0df6474057d6842d
DESARROLLOWGS <wgsdesarrollo@gmail.com>
2026-01-20 10:06:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+function loadPage(page) {
+  fetch('pages/' + page)
+    .then(response => {
+      if (!response.ok) throw new Error("No encontrado");
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('content').innerHTML = data;
+      window.scrollTo(0, 0);
+    })
+    .catch(() => {
+      document.getElementById('content').innerHTML =
+        "<h2>Error</h2><p>Contenido no disponible.</p>";
+    });
+}
 
